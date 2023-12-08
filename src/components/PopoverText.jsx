0cfc699d9544cf3a9315d09d1febb5bb5db88a99
Lincoln Nguyen <nguylinc@amazon.com>
2023-12-08 15:55:29 -0800

    Fix styling for PopoverText

--- src/components/PopoverText.jsx
+++ src/components/PopoverText.jsx
@@ -1,8 +1,10 @@
+import './PopoverText.scss'
+
 export function PopoverText({ children }) {
   return (
-    <div className='cursor-pointer'>
-      <h4 className="m-0">{children}</h4>
-      <div className="w-full border-t-2 border-dashed border-black mt-[1px]" />
+    <div className='popover-text'>
+      {children}
+      <div className='underline' />
     </div>
   )
 }
